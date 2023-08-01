98cf2d2de6d3649eea517d0db8c1d6a0fe3d5a28
orzogc <orzogc@163.com>
2023-08-02 01:33:44 +0800

    update readme

---
+++ README.md
@@ -0,0 +1,1 @@
+[acfunlive-backend](https://github.com/ACFUN-FOSS/acfunlive-backend)的`javascript`和`rescript`接口
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { makeWebAPISession, makeNodeSession, makeTauriSession } from './src/AcFunLiveBackend.bs.mjs';
 
 export const WebAPISession = makeWebAPISession;
+
 export const NodeSession = makeNodeSession;
+
 export const TauriSession = makeTauriSession;
