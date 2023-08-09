93ebf2d6438b64cef300b945fcf8a1ec885712ff
orzogc <orzogc@163.com>
2023-08-09 19:53:11 +0800

    update export

--- tauri.d.ts
+++ tauri.d.ts
@@ -1,8 +1,8 @@
 import { Session } from './src/AcLive__Session';
 import { config } from './src/AcLive__Session.gen';
 
-export { Session } from './src/AcLive__Session';
-export { config } from './src/AcLive__Session.gen';
+export * from './src/AcLive__Session';
+export * from './src/AcLive__Session.gen';
 export * from './src/data/AcLive__Message.gen';
 
 declare const tauriSession: (config?: config) => Session;
