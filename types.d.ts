b505450dc7c43968a3af9c146ba7b8aa92b6755b
orzogc <orzogc@163.com>
2023-11-05 01:18:32 +0800

    add types.js

---
+++ types.d.ts
@@ -0,0 +1,3 @@
+export * from './src/AcLive__Session';
+export * from './src/AcLive__Session.gen.ts';
+export * from './src/data/AcLive__Message.gen.ts';
--- webapi.d.ts
+++ webapi.d.ts
@@ -1,9 +1,6 @@
-import { Session } from './src/AcLive__Session.ts';
-import { config } from './src/AcLive__Session.gen.ts';
+import type { config, Session } from './types.d.ts';
 
-export * from './src/AcLive__Session.ts';
-export * from './src/AcLive__Session.gen.ts';
-export * from './src/data/AcLive__Message.gen.ts';
+export type * from './types.d.ts';
 
 declare const webApiSession: (config?: config) => Session;
 export default webApiSession
