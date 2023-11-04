b4a8499492298a7b00be95e82686c24d19f5ea07
orzogc <orzogc@163.com>
2023-11-05 01:31:01 +0800

    fix types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,3 @@
-export * from './src/AcLive__Session';
-export * from './src/AcLive__Session.gen.ts';
-export * from './src/data/AcLive__Message.gen.ts';
+export type * from './src/AcLive__Session.d.ts';
+export type * from './src/AcLive__Session.gen.ts';
+export type * from './src/data/AcLive__Message.gen.ts';
